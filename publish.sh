9c8f501a5a7e9f21f41184628dcd4430cb68c001
xqli <xiaoqiangli.mail@gmail.com>
2022-07-11 21:28:17 +0800

    0.0.1

---
+++ publish.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+#编译 src 目录下的 typescript source module
+#生成 js module 到 lib 目录
+npm run build:tsc
+
+version=$1;
+jsonFile=package.json;
+jsonContent=`cat package.json`
+
+node > ${jsonFile} <<EOF
+//Read data
+var data = ${jsonContent};
+
+//Manipulate data
+data.version = '${version}';
+
+//Output data
+console.log(JSON.stringify(data));
+
+EOF
+
+git add .
+git commit -m ${version}
+echo ${version};
+npm publish
+git push origin main
